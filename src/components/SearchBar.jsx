8b4b9a90ef43aa10d72385b6bac9adb13e7c0e58
Arshad Arif Ahmed <76579075+arshad10x@users.noreply.github.com>
2022-06-13 12:13:00 +0530

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,29 +1,60 @@
+// import React from "react";
+
+// const SearchBar = ({
+//     handleSubmit,
+//     query,
+//     isLoading,
+//     setQuery
+// }) => {
+//     return (
+//         <form onSubmit={handleSubmit}>
+//             <input 
+//                 value={query}
+//                 className="form-control"
+//                 placeholder="Search Recipe"
+//                 name="query"
+//                 disabled={isLoading}
+//                 onChange={(event) => setQuery(event.target.value)}
+//             />   
+//             <input
+//                 disabled={isLoading || !query}
+//                 type="submit"
+//                 className="btn"
+//                 value="Search"
+//             />
+//         </form>
+//     )
+// };
+
+// export default SearchBar;
+
 import React from "react";
 
-const SearchBar = ({
-    handleSubmit,
-    query,
-    isLoading,
-    setQuery
-}) => {
-    return (
-        <form onSubmit={handleSubmit}>
-            <input 
-                value={query}
-                className="form-control"
-                placeholder="Search Recipe"
-                name="query"
-                disabled={isLoading}
-                onChange={(event) => setQuery(event.target.value)}
-            />   
-            <input
-                disabled={isLoading || !query}
-                type="submit"
-                className="btn"
-                value="Search"
-            />
-        </form>
-    )
+const SearchBar = ({ handleSubmit, query, isLoading, setQuery }) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="SearchComponent">
+        <img className="SearchIcon" alt="Search" src="search-icon.svg" />
+        <input
+          value={query}
+          className="SearchInput"
+          placeholder="Search Recipe"
+          name="query"
+          disabled={isLoading}
+          onChange={(event) => setQuery(event.target.value)}
+        />
+
+        {/* <input
+          disabled={isLoading || !query}
+          type="submit"
+          className="btn"
+          value="Search"
+        /> */}
+      </div>
+    </form>
+  );
 };
 
 export default SearchBar;
+
+
