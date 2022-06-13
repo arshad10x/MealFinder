6d9f4a0c7cf64ed7ceb3819092b5d9e2d31d8d55
Arshad Arif Ahmed <76579075+arshad10x@users.noreply.github.com>
2022-06-13 12:23:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,56 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import "./App.css";
-// import SearchBar from "./components/SearchBar";
-// import RecipeCard from "./components/RecipeCard";
-// const searchApi = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
-
-// function App() {
-//   const [isLoading, setIsLoading] = useState(false);
-//   const [query, setQuery] = useState("");
-//   const [recipes, setRecipes] = useState([]);
-  
-//   // search for the recipe
-//   const searchRecipes = async () => {
-//     setIsLoading(true);
-//     const url = searchApi + query
-//     const res = await fetch(url);
-//     const data = await res.json();
-//     setRecipes(data.meals);
-//     setIsLoading(false);
-//   };
-
-//   useEffect(() => {
-//     searchRecipes()
-//   }, []);
-
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     searchRecipes();
-//   }
-
-//   return (
-//     <div className="container">
-//       <h2>Our Food Recipes</h2>
-//       <SearchBar
-//         isLoading={isLoading}
-//         query={query}
-//         setQuery={setQuery}
-//         handleSubmit={handleSubmit}
-//       />
-//       <div className="recipes">
-//         // No Match
-//         {recipes ? recipes.map(recipe => (
-//           <RecipeCard
-//              key={recipe.idMeal}
-//              recipe={recipe}
-//           />
-//         )) : "No Results."}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
 import React, { useState, useEffect } from "react";
 
 import './App.css';
@@ -93,10 +40,6 @@ const handleSubmit = (event) => {
         query={query}
         setQuery={setQuery}
         handleSubmit={handleSubmit} />
-        {/* <div className="SearchComponent">
-          <img className="SearchIcon" alt="Search" src="search-icon.svg"/>
-          <input className="SearchInput" placeholder="Search Recipe" />
-        </div> */}
       </div>
       <div className="RecipeListContainer">
       {recipes ? recipes.map(recipe => (
