80489c424392fe05949956abb6cf7ef20adeea4c
Arshad Ahmed <76579075+arshad10x@users.noreply.github.com>
2022-07-19 19:03:15 +0530

    Update RecipeContainer.js

--- src/components/RecipeContainer.js
+++ src/components/RecipeContainer.js
@@ -8,7 +8,7 @@ return (
 <img src={strMealThumb} alt={strMeal} className="CoverImage"/>
 <span className= "category"> {strCategory} </ span>
 <h3> {strMeal} </h3>
-<a href={"https://www.themealdb.com/meal/" + idMeal}> Ingredients</a>
+<a href={"https://www.themealdb.com/meal/" + idMeal} target="_blank">Ingredients</a>
 </div>
 </div>
 )
